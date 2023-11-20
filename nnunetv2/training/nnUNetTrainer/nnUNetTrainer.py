dab29f7cf5edb9dff8bbabe3a1432d79daa4d24f
Attila <attila.simko@umu.se>
2023-11-20 16:57:44 +0100

    go

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -1020,8 +1020,15 @@ class nnUNetTrainer(object):
         self.print_to_log_file('val_loss', np.round(self.logger.my_fantastic_logging['val_losses'][-1], decimals=4))
         self.print_to_log_file('Pseudo dice', [np.round(i, decimals=4) for i in
                                                self.logger.my_fantastic_logging['dice_per_class_or_region'][-1]])
+        self.experiment.log_metrics({"training_dice": self.logger.my_fantastic_logging['train_losses'][-1]}, epoch=self.current_epoch)
         self.experiment.log_metrics({"val_dice": self.logger.my_fantastic_logging['val_losses'][-1]}, epoch=self.current_epoch)
         self.experiment.log_metrics({"combined_val_dice":  np.nanmean(self.logger.my_fantastic_logging['dice_per_class_or_region'][-1])}, epoch=self.current_epoch)
+        for label, idx in self.dataset_json["labels"].items():
+            if (np.isnan(self.logger.my_fantastic_logging['dice_per_class_or_region'][-1][idx])):
+                self.experiment.log_metrics({label:  -1}, epoch=self.current_epoch)
+            else:
+                self.experiment.log_metrics({label:  self.logger.my_fantastic_logging['dice_per_class_or_region'][-1][idx]}, epoch=self.current_epoch)
+            print(f"{label} - {idx}")
         self.print_to_log_file(
             f"Epoch time: {np.round(self.logger.my_fantastic_logging['epoch_end_timestamps'][-1] - self.logger.my_fantastic_logging['epoch_start_timestamps'][-1], decimals=2)} s")
 
@@ -1236,7 +1243,7 @@ class nnUNetTrainer(object):
     def run_training(self):
         self.print_to_log_file("Starting comet experiment...")
         self.experiment = Experiment(api_key="ro9UfCMFS2O73enclmXbXfJJj", project_name="miqa")
-
+            
         self.on_train_start()
 
         for epoch in range(self.current_epoch, self.num_epochs):
