a2e1ee6338fd932967e9aa4ace5a0e155914e639
Attila <attila.simko@umu.se>
2023-11-20 16:22:31 +0100

    print

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -1014,10 +1014,13 @@ class nnUNetTrainer(object):
 
     def on_epoch_end(self):
         self.logger.log('epoch_end_timestamps', time(), self.current_epoch)
+
+        print(np.round(self.logger.my_fantastic_logging['val_losses'][-1], decimals=4))
+        print(np.nanmean(self.logger.my_fantastic_logging['dice_per_class_or_region'][-1]))
         
         # todo find a solution for this stupid shit
-        self.experiment.log_metric({"val_dice": np.nanmean(self.logger.my_fantastic_logging['dice_per_class_or_region'][-1])}, epoch=self.current_epoch)
-        self.experiment.log_metric({"combined_val_dice": np.round(self.logger.my_fantastic_logging['val_losses'][-1], decimals=4)}, epoch=self.current_epoch)
+        self.experiment.log_metric({"val_dice": np.round(self.logger.my_fantastic_logging['val_losses'][-1], decimals=4)}, epoch=self.current_epoch)
+        self.experiment.log_metric({"combined_val_dice":  np.nanmean(self.logger.my_fantastic_logging['dice_per_class_or_region'][-1])}, epoch=self.current_epoch)
         self.print_to_log_file('train_loss', np.round(self.logger.my_fantastic_logging['train_losses'][-1], decimals=4))
         self.print_to_log_file('val_loss', np.round(self.logger.my_fantastic_logging['val_losses'][-1], decimals=4))
         self.print_to_log_file('Pseudo dice', [np.round(i, decimals=4) for i in
