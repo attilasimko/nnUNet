42a287f4b13676b994751f3da85b14971d55ea4c
Attila <attila.simko@umu.se>
2023-11-20 14:25:52 +0100

    go

--- remote.sh
+++ remote.sh
@@ -5,8 +5,9 @@
 #SBATCH --error=/cephyr/users/attilas/Alvis/out/%J_error.out
 #SBATCH --output=/cephyr/users/attilas/Alvis/out/%J_output.out
 
-module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
-source /cephyr/users/attilas/Alvis/venv/bin/activate
+module load CUDA/11.7.0
+module load Python/3.9.6-GCCcore-11.2.0
+source /cephyr/users/attilas/Alvis/nnunet/bin/activate
 
 nnUNetv2_train 1 2d 0
 wait
