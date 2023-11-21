f741f2ef00b1a3148ca4dad634d593dce44d136b
Attila <attila.simko@umu.se>
2023-11-21 06:43:18 +0100

    remote

--- remote.sh
+++ remote.sh
@@ -10,5 +10,5 @@ module load CUDA/11.7.0
 module load Python/3.9.6-GCCcore-11.2.0
 source /cephyr/users/attilas/Alvis/nnunet/bin/activate
 
-nnUNetv2_train 1 2d 0
+nnUNetv2_train 1 2d 0 nnUNet_raw="/mimer/NOBACKUP/groups/naiss2023-6-64/nnUNet_raw" nnUNet_preprocessed="/mimer/NOBACKUP/groups/naiss2023-6-64/nnUNet_preprocessed" nnUNet_results="/mimer/NOBACKUP/groups/naiss2023-6-64/nnUNet_results"
 wait
