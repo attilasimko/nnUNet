a688a9dd84ea1c9c0e97d72a138333cb56c01747
Attila <attila.simko@umu.se>
2023-11-21 11:06:50 +0100

    new dice

--- nnunetv2/training/loss/dice.py
+++ nnunetv2/training/loss/dice.py
@@ -120,8 +120,8 @@ class MemoryEfficientSoftDiceLoss(nn.Module):
 
             dc_c = (2 * intersect + self.smooth) / (torch.clip(sum_gt + sum_pred + self.smooth, 1e-8))
 
-            dc += dc_c.mean()
-        return -dc / len(indices)
+            dc += dc_c.mean() / len(indices)
+        return - dc.mean()
 
 
 def get_tp_fp_fn_tn(net_output, gt, axes=None, mask=None, square=False):
